b71324900588106c26d871a4f7de786bfae167d5
Diwa del Mundo <dldelmundo@apper.ph>
2020-06-28 13:48:00 +0000

    Added long at lat randomzier.

--- randomizer.js
+++ randomizer.js
@@ -8,10 +8,21 @@ module.exports = {
 
 // Make sure to "npm install faker" first.
 const Faker = require('faker');
+const baseBookingLong = 14;
+const baseBookingLat = 121;
+
+const baseTargetLong = 14;
+const baseTargetLat = 120;
 
 function generateRandomData(userContext, events, done) {
   const email = Faker.internet.exampleEmail();
   userContext.vars.randomRiderId = uuidv4();
+  
+  userContext.vars.randomBookingLocN = (baseBookingLong + Math.random()).toString().substring(0, 9);
+  userContext.vars.randomBookingLocW = (baseBookingLat + Math.random()).toString().substring(0, 9);
+  
+  userContext.vars.randomTargetLocN = (baseTargetLong + Math.random()).toString().substring(0, 9);
+  userContext.vars.randomTargetLocW = (baseTargetLat + Math.random()).toString().substring(0, 9);
 
   return done();
 }
